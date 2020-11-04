818acfe128572be1f75a078ae38584428fb0701f
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-11-04 20:14:54 +0300

    add user model validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :course_appointments
   has_many :courses, through: :course_appointments
 
-  validates :username, presence: true, length: { in: 3..25, message: 'length should be between 3 and 25' },
-                       uniqueness: { message: 'should be unique' }
+  validates :username, presence: true, length: { in: 3..25 },
+                       uniqueness: {case_sensitive: false  }
 
-  validates :password, presence: true, length: { in: 3..25, message: 'length should be between 3 and 25' }
+  validates :password, presence: true, length: { in: 3..25 }
 end
